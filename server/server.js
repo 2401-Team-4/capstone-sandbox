f6da4a14b3878fc78e62166c4aa77466e8a48b99
Louis-Mascari <Lmascari16@gmail.com>
2024-03-08 19:01:30 -0500

    (bug) - failed fetch requests not being received by server
    Co-authored-by: Erik Wiens <erik.wiens@gmail.com>

--- server/server.js
+++ server/server.js
@@ -11,22 +11,39 @@ const recordedEvents = [];
 
 app.post("/record", (req, res) => {
   const { events } = req.body;
-  console.log("batch of events:", events);
-  let consolePayloads = events.filter((obj) => obj.data.plugin);
-  if (consolePayloads.length > 0) {
-    consolePayloads.forEach((consolePayload) => {
-      console.log("Payload: ", consolePayload);
-    });
-  } else {
-    console.log("No console payload");
-  }
+  // console.log("batch of events:", events);
+  // let consolePayloads = events.filter((obj) => obj.data.plugin);
+  // if (consolePayloads.length > 0) {
+  //   consolePayloads.forEach((consolePayload) => {
+  //     console.log("Payload: ", consolePayload);
+  //   });
+  // } else {
+  //   console.log("No console payload");
+  // }
+
+  // let networkEventPayload = events.filter((obj) => obj.type === 200);
+  // if (networkEventPayload.length > 0) {
+  //   networkEventPayload.forEach((event) => {
+  //     console.log("Network event: ", event);
+  //   });
+  // } else {
+  //   console.log("No network event payload");
+  // }
+
   recordedEvents.push({ events });
-  // console.log("Recorded events:", events);
   res.sendStatus(200);
 });
 
 app.get("/getRecordedEvents", (req, res) => {
   res.json(recordedEvents);
+});
+
+app.get("/random", (req, res) => {
+  res.json("aslkdfgnalksgnalksdgnaksl");
+});
+
+app.delete("/deleteTest", (req, res) => {
+  res.status(305).json("deleted");
 });
 
 app.listen(port, () => {
--- targetApp/src/TargetApp.jsx
+++ targetApp/src/TargetApp.jsx
@@ -5,54 +5,84 @@ const TargetApp = () => {
   let events = [];
 
   const originalFetch = window.fetch;
-  window.fetch = function (url, config) {
-    // Your Fetch interception logic here
+  window.fetch = async (url, config) => {
+    // Request Interceptor
     // Type 200 arbitrarily assigned for us to know it's a network request
     const networkEventObj = { type: 200 };
-
     networkEventObj.data = {
       url: url,
-      method: config.method,
-      headers: config.headers,
-      body: config.body.slice(0, 120),
+      method: config ? config.method : "GET",
+      // headers: config.headers
+      // body: config.body.slice(0, 120),
       type: "FETCH",
       requestMadeAt: Date.now(),
     };
-    // arguments captures arguments to original function, see mdn for more details
-    const result = originalFetch.apply(this, arguments);
 
-    result
-      .then((res) => {
-        const currentTime = Date.now();
-        networkEventObj.timestamp = currentTime;
-        networkEventObj.data.responseReceivedAt = currentTime;
-        networkEventObj.data.latency =
-          networkEventObj.data.responseReceivedAt -
-          networkEventObj.data.requestMadeAt;
+    const response = await originalFetch(url, config);
 
-        networkEventObj.data.status = res.status;
-        // assigning timestamp at this point to ensure network event is pushed to event array in correct order related to other events
-        // need to wait till response received to push the object as we need the status of the response
-        events.push(networkEventObj);
-        return res;
-      })
-      .catch((error) => {
-        console.error("Fetch Error: ", error);
-      });
+    // Response Interceptor
+    const currentTime = Date.now();
+    networkEventObj.timestamp = currentTime;
+    networkEventObj.data.responseReceivedAt = currentTime;
+    networkEventObj.data.latency =
+      networkEventObj.data.responseReceivedAt -
+      networkEventObj.data.requestMadeAt;
+    networkEventObj.data.status = response.status;
 
-    return result;
+    events.push(networkEventObj);
+
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
+    return response;
+    // result
+    //   .then((res) => {
+    //     console.log("got in then");
+    //     if (!res.ok) {
+    //       console.log("res is not okay");
+    //     }
+
+    //     networkEventObj.data.status = res.status;
+    //     // assigning timestamp at this point to ensure network event is pushed to event array in correct order related to other events
+    //     // need to wait till response received to push the object as we need the status of the response
+    //     events.push(networkEventObj);
+    //     // may be redundant, seems can get to the .then handler without the return statement
+    //     return res;
+    //   })
+    //   // .catch(() => {});
+    //   .catch((error) => {
+    //     console.error("Fetch Error: ", error);
+    //     const currentTime = Date.now();
+    //     networkEventObj.timestamp = currentTime;
+    //     networkEventObj.data.responseReceivedAt = currentTime;
+    //     networkEventObj.data.latency =
+    //       networkEventObj.data.responseReceivedAt -
+    //       networkEventObj.data.requestMadeAt;
+
+    //     networkEventObj.data.status = "ERROR";
+    //     console.log("got right before push");
+    //     console.log(networkEventObj);
+    //     events.push(networkEventObj);
+    //   });
+
+    // return result;
   };
 
   useEffect(() => {
     const stopRecording = record({
       emit(event) {
         events.push(event);
-        //
-        //   const defaultLog = console.log["__rrweb_original__"]
-        //     ? console.log["__rrweb_original__"]
-        //     : console.log;
+
+        // const defaultLog = console.log["__rrweb_original__"]
+        //   ? console.log["__rrweb_original__"]
+        //   : console.log;
       },
-      plugins: [getRecordConsolePlugin()],
+      plugins: [
+        getRecordConsolePlugin({
+          stringifyOptions: { stringLengthLimit: 250 },
+        }),
+      ],
     });
 
     const saveEventsInterval = setInterval(save, 10000);
@@ -96,7 +126,24 @@ const TargetApp = () => {
       <button onClick={() => console.error(new Error())}>
         Click for error
       </button>
-      <button onClick={() => console.log(networkRequests)}>Log Requests</button>
+      <button
+        onClick={() =>
+          fetch("http://localhost:3001/random")
+            .then((res) => console.log("successful random"))
+            .catch((rej) => console.log("failure random"))
+        }
+      >
+        Get Random
+      </button>
+      <button
+        onClick={() =>
+          fetch("http://localhost:3001/deleteTest", { method: "DELETE" })
+            .then((res) => console.log("successful delete"))
+            .catch((rej) => console.log("failure delete"))
+        }
+      >
+        Delete
+      </button>
     </>
   );
 };
